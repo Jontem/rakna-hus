c3c8253300f1f7072735eacc7ae864403d7ebbfe
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2019-01-01 21:00:04 +0100

    styled components

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const RootContainer = styled.div`
+  background: red;
+`;
 
 export class Root extends React.Component<{}, {}> {
   render(): JSX.Element {
-    return <div>Hello world3</div>;
+    return <RootContainer>Hello world3</RootContainer>;
   }
 }
